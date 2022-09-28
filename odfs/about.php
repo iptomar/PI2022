18abf6a3e9ba541811f5f53e7be8f00cbebf49f8
JoaoSilva22917 <Aluno22917@ipt.pt>
2022-09-28 15:39:33 +0100

    odfs

--- odfs/about.php
+++
@@ -1,9 +0,0 @@
-<section class="py-5">
-    <div class="container">
-        <div class="card rounded-0">
-            <div class="card-body">
-                <?php include "about.html" ?>
-            </div>
-        </div>
-    </div>
-</section>
